01969cd43228c588779e4423a94dffaed3eb3052
Aqib1422 <mraqibshafique@gmail.com>
2024-05-19 01:56:52 +0500

    navbar

---
+++ resources/views/daraz.blade.php
@@ -0,0 +1,26 @@
+<x-layout>
+
+    <div class="container-fluid" style="background-color: #F85606">
+        <div class="container d-flex flex-column ">
+            <div class="d-flex row flex-row navupper justify-content-between">
+                <div class="col-lg-6">
+                    <ul class="list-unstyled d-flex flex-row gap-4">
+                        <li>Become a Seller</li>
+                        <li>Daraz Affiliate Program</li>
+                        <li>Help & Support</li>
+                        <li>Daraz Logistics</li>
+                    </ul>
+                </div>
+                <div class="col-lg-2">
+                    <button class="border-0" style="background-color: transparent">
+                        <img width="30px" src="{{ URL::to('/assets/images/daraz.png') }}" alt="">
+                        Save more on App
+                    </button>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </div>
+</x-layout>
