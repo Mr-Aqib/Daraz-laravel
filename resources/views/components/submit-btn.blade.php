b536605945391463b3e80d66551c11aea2a7a364
Aqib1422 <mraqibshafique@gmail.com>
2024-05-23 02:51:27 +0500

    add cate

---
+++ resources/views/components/submit-btn.blade.php
@@ -0,0 +1,4 @@
+<button style="background:linear-gradient(135deg,#F36D21,#F29D21)"
+    class="my-4 fw-medium fs-5 p-1 rounded-3 border-0 w-75 mx-auto d-block">
+    {{ $slot }}
+</button>
--- resources/views/pages/seller/add-cate.blade.php
+++ resources/views/pages/seller/add-cate.blade.php
@@ -1,5 +1,15 @@
 <x-seller-component>
-    <div class="container">
+    <div class="container col-lg-5">
         <h1 class="text-center">Add categories</h1>
+        <form class="shadow p-3 rounded-4" action="" method="POST" enctype="multipart/form-data">
+            <label for="">Category</label>
+            <input type="text" name="category" class="form-control">
+            <label for="">Image</label>
+            <input type="file" name="image" class="form-control" id="">
+            <x-submit-btn>
+                Submit
+            </x-submit-btn>
+        </form>
     </div>
+
 </x-seller-component>
